75f5656456ed451945e3af3e0e806cad938af96f
kylegarrettwilson <LetItBe@c-67-201-244-42.reshall.wwu.edu>
2015-09-24 16:31:12 -0700

    work

--- Final Project/js/main.js
+++ Final Project/js/main.js
@@ -7,7 +7,7 @@ window.onload = function() {
             if (ctx) {
 
 
-
+                /* Canvas */
 
 
 
@@ -57,38 +57,125 @@ window.onload = function() {
 
 
 
+    /* Two jQuery effects */
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    var state = true;
+    $( "#content" ).hover(function() {
+        if ( state ) {
+            $( "img" ).animate({marginTop: "5%", opacity: "0.6"}, 1000 );
+        } else {
+            $( "img" ).animate({marginTop: "0", opacity: "1"}, 1000 );
+        }
+        state = !state;
+    });
 
 
 };
+
+
+
+
+
+
+
+
+    /* Bar Graph of Views */
+
+
+
+    $(document).ready(function(){
+
+        window.chartHeight = Number($(".chart-area").height());
+        window.barWidth = $(".chart-area .chart-bar").width();
+        window.highestYlabel = Number($(".chart-y-axis p").first().html());
+        window.chartHeightArea = window.chartHeight - Number($("p.axis-value").first().height());
+        window.chartScale = chartHeightArea / window.highestYlabel;
+        window.barSpacing = Number($(".chart-area").attr("bar-spacing"));
+        positionGraph();
+
+    });
+
+    function positionGraph(){
+        $(".chart-area .chart-bar").each(function(index){
+
+            var barPosition = (window.barWidth * index) + (index * barSpacing) + barSpacing;
+            $(this).css("left",barPosition + "px");
+            $(this).html("<p>" + $(this).attr("bar-value") + "</p>");
+            $(".chart-x-axis").append('<p style="left:'+(barPosition-(window.barWidth/2))+'px;">'+$(this).attr('label')+'</p>');
+
+            var barValue = Number($(this).attr("bar-value"));
+            if(barValue > window.maxValue){
+                window.maxValue = barValue;
+                window.valueMultiplier = window.maxValue / window.highestYlabel;
+            }
+
+        });
+        animateGraph();
+    }
+
+    function animateGraph(){
+
+        $(".chart-area .chart-bar").each(function(index){
+            var revisedValue = Number($(this).attr("bar-value")) * window.chartScale;
+            var newDelay = 150 * index;
+            $(this).delay(newDelay).animate({height:revisedValue}, 1500, function(){
+                $(this).children('p').delay(400).fadeIn(100);
+            });
+        });
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
